4b6fce96fa8661f45ab50da91f804a73cc40e0a1
Jakob K <dev@jakobk.de>
2022-12-20 21:01:06 +0100

    filter: Add include filter

---
+++ filters/include.hs
@@ -0,0 +1,20 @@
+import Data.Text qualified as T
+import Data.Text.IO qualified as TIO
+import System.FilePath (takeExtension)
+import Text.Pandoc.JSON
+
+doInclude :: Block -> IO Block
+doInclude cb@(CodeBlock (id, classes, namevals) contents) =
+  case lookup (T.pack "include") namevals of
+    Just f -> do
+      let fileExtension =
+            if null classes
+              then T.pack (takeExtension (T.unpack f))
+              else head classes
+      CodeBlock (id, [fileExtension], namevals)
+        <$> TIO.readFile (T.unpack f)
+    Nothing -> return cb
+doInclude x = return x
+
+main :: IO ()
+main = toJSONFilter doInclude
