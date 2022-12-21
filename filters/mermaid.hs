e7e66fd3a2baf76781adc6799a1ca899c6f37fbf
Jakob K <dev@jakobk.de>
2022-12-21 21:59:25 +0100

    filter: Some more unfinished mermaid filter stuff

--- filters/mermaid.hs
+++ filters/mermaid.hs
@@ -3,12 +3,13 @@ import System.Directory
 import System.IO
 import System.Process
 import Text.Pandoc.JSON
-
--- TODO not finished
+import qualified Data.ByteString
+import qualified Data.ByteString.Base64
 
 renderMermaid :: Block -> IO Block
 renderMermaid cb@(CodeBlock (id, classes, namevals) contents) | T.pack ".mermaid" `elem` classes = do
-  tempDir <- getTemporaryDirectory
+  systemTempDir <- getTemporaryDirectory
+  let tempDir = systemTempDir + "/mermaid-tmp"
   -- create a temporary file to write the input to
   (tempFileIn, tempHandleIn) <- openTempFile tempDir "mermaid-temp-input.svg"
   -- create a temporary file to save the svg to
@@ -17,9 +18,13 @@ renderMermaid cb@(CodeBlock (id, classes, namevals) contents) | T.pack ".mermaid
   -- execute mermaid-cli
   readProcess "mmdc" ["-i", tempFileIn, "-o", tempFileOut] ""
 
+  outputContent <- Data.ByteString.readFile tempFileOut
+  let base64content = Data.ByteString.Base64.encodeBase64 outputContent
+
   removeDirectory tempDir
 
-  cb
+  --                          \/ TODO allow to pass classes
+  return Para [Image (id, [], []) [Str (T.pack "Test Caption")] (base64content, T.pack "fig:title")]
 renderMermaid x = return x
 
 main :: IO ()
