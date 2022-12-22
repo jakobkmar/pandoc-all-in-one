fe8725f6dfc8415b71b5b6789f5d0b26710dab69
Jakob K <dev@jakobk.de>
2022-12-22 05:12:06 +0100

    filter: mermaid.hs implement read file base64 function

--- filters/mermaid.hs
+++ filters/mermaid.hs
@@ -1,30 +1,35 @@
+import Data.ByteString qualified as BS
+import Data.ByteString.Base64 qualified as B64
 import Data.Text qualified as T
 import System.Directory
 import System.IO
 import System.Process
 import Text.Pandoc.JSON
-import qualified Data.ByteString
-import qualified Data.ByteString.Base64
+
+readFileBase64 :: FilePath -> IO T.Text
+readFileBase64 filePath = do
+  bsFileContent <- BS.readFile filePath
+  return (B64.encodeBase64 bsFileContent)
 
 renderMermaid :: Block -> IO Block
-renderMermaid cb@(CodeBlock (id, classes, namevals) contents) | T.pack ".mermaid" `elem` classes = do
-  systemTempDir <- getTemporaryDirectory
-  let tempDir = systemTempDir + "/mermaid-tmp"
-  -- create a temporary file to write the input to
-  (tempFileIn, tempHandleIn) <- openTempFile tempDir "mermaid-temp-input.svg"
-  -- create a temporary file to save the svg to
-  (tempFileOut, tempHandleOut) <- openTempFile tempDir "mermaid-temp-output.svg"
+renderMermaid cb@(CodeBlock (id, classes, namevals) contents)
+  | T.pack ".mermaid" `elem` classes = do
+      systemTempDir <- getTemporaryDirectory
+      let tempDir = systemTempDir ++ "/mermaid-tmp"
+      -- create a temporary file to write the input to
+      (tempFileIn, tempHandleIn) <- openTempFile tempDir "mermaid-temp-input.svg"
+      -- create a temporary file to save the svg to
+      (tempFileOut, tempHandleOut) <- openTempFile tempDir "mermaid-temp-output.svg"
 
-  -- execute mermaid-cli
-  readProcess "mmdc" ["-i", tempFileIn, "-o", tempFileOut] ""
+      -- execute mermaid-cli
+      readProcess "mmdc" ["-i", tempFileIn, "-o", tempFileOut] ""
 
-  outputContent <- Data.ByteString.readFile tempFileOut
-  let base64content = Data.ByteString.Base64.encodeBase64 outputContent
+      base64Content <- readFileBase64 tempFileOut
 
-  removeDirectory tempDir
+      removeDirectory tempDir
 
-  --                          \/ TODO allow to pass classes
-  return Para [Image (id, [], []) [Str (T.pack "Test Caption")] (base64content, T.pack "fig:title")]
+      --                           \/ TODO allow to pass classes
+      return (Para [Image (id, [], []) [Str (T.pack "Test Caption")] (base64Content, T.pack "fig:title")])
 renderMermaid x = return x
 
 main :: IO ()
