f4573cba828a81f81a275b453738de46b40d2c54
Jakob K <dev@jakobk.de>
2022-12-24 05:03:58 +0100

    entrypoint: Allow watching the input file

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM haskell:latest as builder
 
 RUN cabal update && cabal install --lib pandoc-types base64
 
-COPY . .
+COPY filters filters
 RUN ./filters/compile.sh
 
 
@@ -19,13 +19,15 @@ RUN dnf install -y chromium yarnpkg
 
 RUN yarnpkg global add @mermaid-js/mermaid-cli
 
+# tools
+RUN dnf install -y inotify-tools
 # conversion
 RUN dnf install -y librsvg2 librsvg2-tools ghostscript wkhtmltopdf R-rsvg
 
-COPY --from=builder filters filters/
+COPY --from=builder filters /filters/
 
-COPY docker/resources resources/
-COPY docker/entrypoint.sh run/
+COPY docker/resources /resources/
+COPY docker/entrypoint.sh /run/
 RUN chmod +x /run/entrypoint.sh
 
 WORKDIR /data
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 
-pandoc \
-  -F /filters/mermaid \
-  -F /filters/include \
-  "$@"
+for arg do
+  shift
+  if [ "$arg" = "--watch" ]; then
+    watchForChanges=true
+    continue
+  else
+    # FIXME this is unstable, and only works if the input file comes first TODO
+    if ! [[ $arg == "-*" ]] && ! [[ $arg == "--*" ]]; then
+      if [ -z "$watchFile" ]; then
+        watchFile=$arg
+      fi
+    fi
+    set -- "$@" "$arg"
+  fi
+done
+
+runPandoc() {
+  pandoc \
+    -F /filters/mermaid \
+    -F /filters/include \
+    "$@"  
+}
+
+if [[ $watchForChanges == true ]]; then
+  echo "Running pandoc and watching for changes of file $watchFile"
+  while inotifywait -e close_write "$watchFile"; do
+    runPandoc "$@"
+  done
+else
+  runPandoc "$@"
+fi
